ecad19988274536e7fa1c0f8ba1c9fe23211e6e8
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-12-23 16:21:46 +0530

    adding the cors

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: '*',
+        origin: ['https://mytalor-zone-lar6.vercel.app/','http://localhost:5173/'],
         methods: ["GET", "POST", "DELETE", "PUT"],
         credentials: true,
     })
